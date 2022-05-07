bda9e2c9438c37ab02091a22ec2faa6facca788f
thehejian <745544752@qq.com>
2022-05-08 01:04:49 +0800

    Create 009-fofa查询（不用翻墙）.sh

---
+++ 009-fofa查询（不用翻墙）.sh
@@ -0,0 +1,22 @@
+fofa网址
+https://fofa.info/
+
+标题
+title="beijing" && country="CN"
+title="何健" && country="CN"
+
+网站
+domain="xuegod.cn" && region!="HK"
+
+IP
+ip="220.181.111.1/24"
+ip="47.112.144.203"
+
+JS名称
+js_name="//g.alicdn.com/sd/baxia/2.0.62/baxiaCommon.js"
+
+规则列表
+https://fofa.info/library
+
+查看视频设备
+app="EdmWebVideo" && country="CN" && region="Liaoning"
